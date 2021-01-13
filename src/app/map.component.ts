7f852848ab46edf93f3cab84cfc5cab665ae651a
Leonardo <lgspezia@yahoo.com.br>
2021-01-13 01:35:27 -0300

    map correction
    map enhancement

---
+++ src/app/map.component.ts
@@ -0,0 +1,215 @@
+import { Component, Input, ViewChild, NgZone, OnInit } from '@angular/core';
+import { MapsAPILoader, AgmMap, GoogleMapsAPIWrapper } from '@agm/core';
+// import { GoogleMapsAPIWrapper } from '@agm/core/services';
+
+declare var google: any;
+
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+  viewport?: Object;
+  zoom: number;
+  address_level_1?:string;
+  address_level_2?: string;
+  address_country?: string;
+  address_zip?: string;
+  address_state?: string;
+  marker?: Marker;
+  markers?: Marker[];
+}
+
+interface Vehicle {
+  lat: number;
+  lng: number;
+  placa: string;
+  data: string;
+}
+
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+
+export class MapComponent implements OnInit {
+  circleRadius:number = 3500; // km
+  geocoder:any;
+  dataatual = new Date();
+
+  public location:Location = {
+    lat: -30,
+    lng: -60,
+    marker: {
+      lat: -30.01,
+      lng: -60.02,
+      draggable: false
+    },
+    markers: [{
+      lat: -30.01,
+      lng: -60.02,
+      draggable: false
+    },
+    {
+      lat: -30.03,
+      lng: -59.99,
+      draggable: false
+    },
+  ],
+    zoom: 12
+  };
+
+  public vehicle:Vehicle = {
+    lat: 51.76980205405271,
+    lng: 25.26496636999715,
+    placa: "AVO-0372",
+    data: this.dataatual.toUTCString(),
+  };
+
+public vehicle2:Vehicle = {
+  lat: 51.6980205405271,
+  lng: 25.46496636999715,
+  placa: "AVO-0350",
+  data: this.dataatual.toUTCString(),
+};
+
+  @ViewChild(AgmMap) map: AgmMap;
+
+  constructor(public mapsApiLoader: MapsAPILoader,
+              private zone: NgZone,
+              private wrapper: GoogleMapsAPIWrapper) {
+    this.mapsApiLoader = mapsApiLoader;
+    this.zone = zone;
+    this.wrapper = wrapper;
+    this.mapsApiLoader.load().then(() => {
+      this.geocoder = new google.maps.Geocoder();
+    });
+  }
+
+  ngOnInit() {
+      this.location.marker.draggable = true;
+  }
+
+  findLocation(address) {
+    if (!this.geocoder) this.geocoder = new google.maps.Geocoder()
+    this.geocoder.geocode({
+      'address': address
+    }, (results, status) => {
+      console.log(results);
+      if (status == google.maps.GeocoderStatus.OK) {
+        for (var i = 0; i < results[0].address_components.length; i++) {
+          let types = results[0].address_components[i].types
+          console.log(types);
+          if (types.indexOf('locality') != -1) {
+            this.location.address_level_2 = results[0].address_components[i].long_name
+          }
+          if (types.indexOf('country') != -1) {
+            this.location.address_country = results[0].address_components[i].long_name
+          }
+          if (types.indexOf('postal_code') != -1) {
+            this.location.address_zip = results[0].address_components[i].long_name
+          }
+          if (types.indexOf('administrative_area_level_1') != -1) {
+            this.location.address_state = results[0].address_components[i].long_name
+          }
+        }
+        if (results[0].geometry.location) {
+          this.location.lat = results[0].geometry.location.lat();
+          this.location.lng = results[0].geometry.location.lng();
+          this.location.marker.lat = results[0].geometry.location.lat();
+          this.location.marker.lng = results[0].geometry.location.lng();
+          this.location.marker.draggable = true;
+          this.location.viewport = results[0].geometry.viewport;
+        }
+
+        this.map.triggerResize()
+      } else {
+        alert("Sorry, this search produced no results.");
+      }
+    })
+  }
+
+  findAddressByCoordinates() {
+    this.geocoder.geocode({
+      'location': {
+        lat: this.location.marker.lat,
+        lng: this.location.marker.lng
+      }
+    }, (results, status) => {
+      this.decomposeAddressComponents(results);
+    })
+  }
+
+  decomposeAddressComponents(addressArray) {
+    if (addressArray.length == 0) return false;
+    let address = addressArray[0].address_components;
+    console.log(address);
+    for(let element of address) {
+      console.log(element);
+      if (element.length == 0 && !element['types']) continue
+      if (element['types'].indexOf('street_number') > -1) {
+        this.location.address_level_1 = element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('route') > -1) {
+        this.location.address_level_1 += ', ' + element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('locality') > -1) {
+        this.location.address_level_2 = element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('administrative_area_level_1') > -1) {
+        this.location.address_state = element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('country') > -1) {
+        this.location.address_country = element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('postal_code') > -1) {
+        this.location.address_zip = element['long_name'];
+        continue;
+      }
+    }
+  }
+
+  updateOnMap() {
+    console.log('updating')
+    let full_address:string = this.location.address_level_1 || ""
+    if (this.location.address_level_2) full_address = full_address + " " + this.location.address_level_2
+    if (this.location.address_state) full_address = full_address + " " + this.location.address_state
+    if (this.location.address_country) full_address = full_address + " " + this.location.address_country
+    this.findLocation(full_address);
+
+  }
+
+  kmToRadius(value) {
+    this.circleRadius = value;
+ }
+
+  circleRadiusInKm() {
+    return this.circleRadius / 1000;
+  }
+
+  milesToRadius(value) {
+     this.circleRadius = value / 0.00062137;
+  }
+
+  circleRadiusInMiles() {
+    return this.circleRadius * 0.00062137;
+  }
+
+  markerDragEnd(m: any, $event: any) {
+   this.location.marker.lat = m.coords.lat;
+   this.location.marker.lng = m.coords.lng;
+   this.findAddressByCoordinates();
+  }
+}
